30a9c0a148e1305a40be6fa4f5ba9c864beffb1e
angel <angelsol299@hotmail.com>
2019-04-28 23:43:11 +0200

    Updates

---
+++ precache-manifest.5c93e5cf69e4f5884a2548cfa10f1855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb26a0e9307d93da39c7",
+    "url": "/portfolio/static/css/main.6ed88424.chunk.css"
+  },
+  {
+    "revision": "eb26a0e9307d93da39c7",
+    "url": "/portfolio/static/js/main.eb26a0e9.chunk.js"
+  },
+  {
+    "revision": "9213290495bc6ba91417",
+    "url": "/portfolio/static/css/1.090cbc43.chunk.css"
+  },
+  {
+    "revision": "9213290495bc6ba91417",
+    "url": "/portfolio/static/js/1.92132904.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "e20946093b020333968198c12fd4d3fc",
+    "url": "/portfolio/index.html"
+  }
+];
